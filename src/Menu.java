dc46ace3cc75b0276c6fe5ca960a864dc1345556
Adrian Vargas <adrianfelipevr@gmail.com>
2025-05-12 14:38:22 -0500

    Add solicitarMoneda, solicitarCantidad methods with his validation

--- src/Menu.java
+++ src/Menu.java
@@ -15,31 +15,59 @@ public class Menu {
         do {
             System.out.println("***** BIENVENIDO AL CONVERTIDOR DE MONEDAS *****");
             System.out.println("1. Convertir Moneda");
-            System.out.println("2. Salir");
+            System.out.println("2. Obtener Tasas de Cambio");
+            System.out.println("3. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = sc.nextInt();
 
             switch (opcion) {
-                case 1:
-                    realizarConversion();
-                    break;
-                case 2:
-                    System.out.println("Finalizando programa...");
-                    break;
-                default:
-                    System.out.println("Opción no valida.");
+                case 1 -> realizarConversion();
+                case 2 -> tasaDeCambio();
+                case 3 -> System.out.println("Finalizando programa...");
+                default -> System.out.println("Opción no valida.");
             }
-        } while (opcion != 2);
+        } while (opcion != 3);
     }
 
-    private  void realizarConversion() {
-        System.out.print("Ingrese codigo de moneda de origen (Ej: USD): ");
-        String monedaOrigen = sc.next().toUpperCase();
-        System.out.print("Ingrese codigo de moneda de destino (Ej: EU): ");
-        String monedaDestino = sc.next().toUpperCase();
-        System.out.print("Ingrese valor a convertir: ");
-        double valor = sc.nextDouble();
+    private void realizarConversion() {
+        String monedaOrigen = solicitarMoneda("Ingrese código de moneda de origen (Ej: USD): ");
+        String monedaDestino = solicitarMoneda("Ingrese código de moneda de destino (Ej: EUR): ");
+        double cantidad = solicitarCantidad("Ingrese cantidad (" + monedaOrigen + ") para convertir a (" + monedaDestino + "): ");
 
-        System.out.println(valor + " " + monedaOrigen + " equivalen a " + "00.00 " + monedaDestino);
+        System.out.println("Conversión lista: " + cantidad + " " + monedaOrigen + " → " + monedaDestino);
+    }
+
+    private String solicitarMoneda(String mensaje) {
+        String moneda;
+        do {
+            System.out.print(mensaje);
+            moneda = sc.next().toUpperCase();
+            if (!consultaAPI.verificarMoneda(moneda)) {
+                System.out.println("La moneda ingresada NO es válida. Intente nuevamente.");
+            }
+        } while (!consultaAPI.verificarMoneda(moneda));
+        return moneda;
+    }
+
+    private double solicitarCantidad(String mensaje) {
+        double cantidad;
+        do {
+            System.out.print(mensaje);
+            if (sc.hasNextDouble()) {
+                cantidad = sc.nextDouble();
+                if (cantidad > 0) {
+                    return cantidad;
+                } else {
+                    System.out.println("El valor debe ser mayor a 0.");
+                }
+            } else {
+                System.out.println("Valor numérico inválido.");
+                sc.next();
+            }
+        } while (true);
+    }
+
+    private void tasaDeCambio() {
+        System.out.println("Tasa de Cambio");
     }
 }
